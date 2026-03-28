4fba32288b881ee92d75f7d45d35ed84aad0b5c1
cji646 <cji6463@gmail.com>
2026-03-28 19:21:17 +0900

    Update supabase.ts

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
+// Vercel 설정창의 이름과 100% 일치시켰습니다.
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
