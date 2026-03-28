21c01b1f36ecd781ece706cee9da94c5b1312048
cji646 <cji6463@gmail.com>
2026-03-28 18:47:14 +0900

    Delete supabase.ts

--- supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// 준일님의 수파베이스 정보
-const supabaseUrl = 'https://jojabvihldirdxywlffg.supabase.co';
-const supabaseKey = ''; 
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
