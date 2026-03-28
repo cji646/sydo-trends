f0b98039f47b9082e9ef14960f9609fa3af9fdd4
cji646 <cji6463@gmail.com>
2026-03-28 19:05:14 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    // 💡 외부 이미지 주소를 허용해서 최적화 기능을 활성화합니다.
-    remotePatterns: [
-      { protocol: 'https', hostname: '**.a-bly.com' },
-      { protocol: 'https', hostname: '**.musinsa.com' },
-      { protocol: 'https', hostname: '**.supabase.co' },
-      { protocol: 'https', hostname: 'jojabvihldirdxywlffg.supabase.co' },
-    ],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
